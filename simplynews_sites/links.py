1e090af04ad5f4ec1341952b6db32798d39bc431
metalune <metalune@mailbox.org>
2021-02-03 12:40:14 +0100

    Add support for shacknews

--- simplynews_sites/links.py
+++ simplynews_sites/links.py
@@ -7,8 +7,12 @@ from . import itsfoss
 from . import decentralizetoday
 from . import nypost
 from . import theguardian
+from . import shacknews
 
 sites = {
+    "shacknews.com": shacknews,
+    "www.shacknews.com": shacknews,
+
     "gameinformer.com": gameinformer,
     "www.gameinformer.com": gameinformer,
 
---
+++ simplynews_sites/shacknews.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import timedelta
+from .helpers import rss
+
+cache_refresh_time_delta = timedelta(hours=12)
+identifier = "shacknews"
+base_url = "https://shacknews.com"
+
+site_title = "Shack News"
+
+rss_feed = f"{base_url}/feed/rss"
+
+
+def get_page(url):
+    full_url = f"{base_url}/" + url
+    soup = BeautifulSoup(requests.get(full_url).text, "lxml")
+
+    data = {
+        'title': soup.find('h1', class_='article-title').text,
+        'subtitle': soup.find('div', class_='article-lead-bottom-content').find('description').text,
+        'author': soup.find('div', class_='article-lead-bottom').find('div', class_='by-line').find('a').text,
+        'last_updated': soup.find('div', class_='time-stamp').text
+    }
+
+    c = []
+
+    for element in soup.find('section', class_='article-content').find('main'):
+        el = {}
+        if element.name == 'p':
+            vid = element.find('iframe')
+            if vid is not None:
+                print()
+                el['type'] = 'iframe'
+                el['src'] = vid['src']
+            else:
+                el['type'] = 'paragraph'
+                el['value'] = element.text
+        else:
+            if element.name is not None:
+                print("Ignoring:", element.name)
+            el = None
+
+        if el is not None:
+            c.append(el)
+            
+    data['article'] = c
+
+    return data
+
+
+def get_recent_articles():
+    return rss.default_feed_parser(rss_feed)
+
+
+if __name__ == "__main__":
+    print(get_recent_articles())
+    #get_page("2021/01/30/john-chaneys-kindness-wont-be-forgotten/")
