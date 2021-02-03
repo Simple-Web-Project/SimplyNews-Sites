949217fbb63c2c9d4ad106e3459dddfec2eceff9
metalune <metalune@mailbox.org>
2021-02-03 09:18:36 +0100

    Add load_sites.py to package

---
+++ simplynews_sites/load_sites.py
@@ -0,0 +1,30 @@
+from . import gameinformer, makeuseof, androidauthority, heise, itsfoss, decentralizetoday,  nypost, theguardian, theverge
+
+sites = {
+    "gameinformer.com": gameinformer,
+    "www.gameinformer.com": gameinformer,
+
+    "makeuseof.com": makeuseof,
+    "www.makeuseof.com": makeuseof,
+
+    "androidauthority.com": androidauthority,
+    "www.androidauthority.com": androidauthority,
+
+    "heise.de": heise,
+    "www.heise.de": heise,
+
+    "theverge.com": theverge,
+    "www.theverge.com": theverge,
+
+    "itsfoss.com": itsfoss,
+    "www.itsfoss.com": itsfoss,
+
+    "dt.gl": decentralizetoday,
+
+    "nypost.com": nypost,
+    "www.nypost.com": nypost,
+
+    "theguardian.com": theguardian,
+    "www.theguardian.com": theguardian,
+}
+
