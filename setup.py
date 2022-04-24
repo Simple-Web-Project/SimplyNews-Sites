d4454f7d8b8d24f2275978337e4c0b48c2bbde05
ManeraKai <manerakai@protonmail.com>
2022-04-24 04:07:35 +0300

    Improved aljazeera.net more

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "simplynews_sites",
-    version = "0.0.3",
+    version = "0.0.4",
     url = "https://codeberg.org/SimpleWeb/SimplyNews-Sites",
 
     license = "AGPLv3 or later",
--- simplynews_sites/aljazeeranet.py
+++ simplynews_sites/aljazeeranet.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import time
 from colorama import Fore, Back, Style
 from pyvirtualdisplay import Display
@@ -25,148 +26,161 @@ site_dir = "rtl"
 
 rss_feed = f"{base_url}/aljazeerarss/a7c186be-1baa-4bd4-9d80-a84db769f779/73d0e1b4-532f-45ef-b135-bfdff8b8cab9"
 
-profile = webdriver.FirefoxProfile()
 
-profile.set_preference("general.useragent.override",
-                       "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/537.36 (KHTML, like Gecko; Mediapartners-Google) Chrome/89.0.4389.130 Mobile Safari/537.36")
-
-# display = Display(visible=0, size=(800, 600))
-# display.start()
-driver = webdriver.Firefox(profile, executable_path='./drivers/geckodriver')
+display = Display(visible=0, size=(800, 600))
+display.start()
+options = Options()
+profile = FirefoxProfile()
+profile.set_preference(
+    "general.useragent.override",
+    "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/537.36 (KHTML, like Gecko; Mediapartners-Google) Chrome/89.0.4389.130 Mobile Safari/537.36"
+)
+options.profile = profile
+# options.page_l    oad_strategy = 'eager'
+driver = webdriver.Firefox(
+    options=options,
+    executable_path='./drivers/geckodriver'
+)
 
 # https://www.aljazeera.net/aljazeerarss/a7c186be-1baa-4bd4-9d80-a84db769f779/73d0e1b4-532f-45ef-b135-bfdff8b8cab9
 
 
 def get_page(url):
-    driver.get(f"{base_url}/{url}")
-    time.sleep(3)
+    try:
+        print(
+            Fore.GREEN + 'Fetching ' + Style.RESET_ALL +
+            f"{base_url}/{urllib.parse.unquote(url)}"
+        )
+        driver.get(f"{base_url}/{url}")
 
-    title = driver.find_element(
-        By.CLASS_NAME, 'article-header').find_element(By.TAG_NAME, 'h1').text
-
-    article = []
-
-    for div in driver.find_elements(By.CLASS_NAME, 'aj-video-player'):
-        video = div.find_element(
+        # Header title
+        title = driver.find_element(
             By.XPATH,
-            '//video[@class="vjs-tech"]')
-        article.append({
-            "type": "video",
-            "src": video.get_attribute("src"),
-        })
-        print(video.get_attribute("src"))
+            '//header[@class="article-header"]/h1'
+        ).text
 
-    for figure in driver.find_elements(By.XPATH, '//figure[@class="article-featured-image"]'):
-        image = figure.find_element(By.TAG_NAME, 'img')
-        article.append({
-            'type': 'image',
-            'src': image.get_attribute('src'),
-            'alt': image.get_attribute('alt')
-        })
-        break
+        article = []
 
-    _article_excerpt = driver.find_elements(By.CLASS_NAME, 'article-excerpt')
-    if len(_article_excerpt) > 0 and _article_excerpt[0].text.strip() != '':
-        article.append({
-            "type": "paragraph",
-            "value": _article_excerpt[0].text
-        })
+        # Header Video
+        for video in driver.find_elements(By.XPATH, '//figure[@class="article-featured-video"]//video'):
+            article.append({
+                "type": "video",
+                "src": video.get_attribute("src"),
+            })
 
-    for element in driver.find_element(By.CLASS_NAME, 'wysiwyg').find_elements(By.XPATH, ".//*"):
-        if element.tag_name == "p" and len(element.find_elements(By.TAG_NAME, 'img')) > 0:
-            image = element.find_elements(By.TAG_NAME, 'img')[0]
+        # Header image
+        for image in driver.find_elements(By.XPATH, '//figure[@class="article-featured-image"]//img'):
             article.append({
                 'type': 'image',
-                'src': base_url + image.get_attribute('src'),
+                'src': image.get_attribute('src'),
                 'alt': image.get_attribute('alt')
             })
-        elif element.tag_name == 'figure':
-            images = element.find_elements(By.TAG_NAME, 'img')
-            if len(images) > 0:
+
+        # Small summary body
+        _article_excerpt = driver.find_elements(By.CLASS_NAME, 'article-excerpt')
+        if len(_article_excerpt) > 0 and _article_excerpt[0].text.strip() != '':
+            article.append({
+                "type": "paragraph",
+                "value": _article_excerpt[0].text
+            })
+
+        for element in driver.find_elements(By.XPATH, '//div[contains(@class, "wysiwyg")]/*'):
+            if element.tag_name == "p" and len(element.find_elements(By.TAG_NAME, 'img')) > 0:
+                image = element.find_element(By.TAG_NAME, 'img')
                 article.append({
                     'type': 'image',
-                    'src': images[0].get_attribute('src'),
-                    'alt': images[0].get_attribute('alt')
+                    'src': base_url + image.get_attribute('src'),
+                    'alt': image.get_attribute('alt')
                 })
-            video = element.find_elements(By.CLASS_NAME, 'vjs-tech')
-            if len(video) > 0:
+            elif element.tag_name == 'figure':
+                images = element.find_elements(By.TAG_NAME, 'img')
+                if len(images) > 0:
+                    article.append({
+                        'type': 'image',
+                        'src': images[0].get_attribute('src'),
+                        'alt': images[0].get_attribute('alt')
+                    })
+                video = element.find_elements(By.CLASS_NAME, 'vjs-tech')
+                if len(video) > 0:
+                    article.append({
+                        'type': 'video',
+                        "src": video[0].get_attribute("src"),
+                    })
+            elif element.tag_name == "p":
+                if len(element.find_elements(By.TAG_NAME, 'a')) > 0:
+                    myList = re.findall(
+                        r"(?:(.*)<a.*href=\"(.*)\">(.*)<\/a>(.*)){1,}",
+                        element.get_attribute('innerHTML')
+                    )
+                    children = []
+                    for i in range(0, len(myList[0])):
+                        if i % 3 == 0:
+                            children.append({
+                                "type": "paragraph",
+                                "value": myList[0][i]
+                            })
+                        elif i % 3 == 1:
+                            children.append({
+                                "type": "link",
+                                "href": myList[0][i],
+                                "value": myList[0][i+1],
+                            })
+                    article.append({
+                        "type": "paragraph_advanced",
+                        "children": children
+                    })
+                else:
+                    article.append({
+                        "type": "paragraph",
+                        "value": element.text,
+                    })
+            elif element.tag_name in ("h1", "h2", "h3", "h4", "h5", "h6"):
                 article.append({
-                    'type': 'video',
-                    "src": video[0].get_attribute("src"),
+                    "type": "header",
+                    "size": element.tag_name,
+                    "value": element.text
                 })
-        elif element.tag_name == "p":
-            if len(element.find_elements(By.TAG_NAME, 'a')) > 0:
-                # "".join([str(x) for x in element.contents]))
-                myList = re.findall(
-                    r"(?:(.*)<a.*href=\"(.*)\">(.*)<\/a>(.*)){1,}",
-                    element.get_attribute('innerHTML')
-                )
-                children = []
-                for i in range(0, len(myList[0])):
-                    if i % 3 == 0:
-                        children.append({
-                            "type": "paragraph",
-                            "value": myList[0][i]
-                        })
-                    elif i % 3 == 1:
-                        children.append({
-                            "type": "link",
-                            "href": myList[0][i],
-                            "value": myList[0][i+1],
-                        })
+            elif element.tag_name == 'div' and 'twitter-tweet' in element.get_attribute("class").split():
                 article.append({
-                    "type": "paragraph_advanced",
-                    "children": children
+                    "type": "iframe",
+                    "src":  element.find_element(By.TAG_NAME, 'iframe').get_attribute('src').replace('platform.twitter.com', 'nitter.pussthecat.org'),
+                    "width": 500,
+                    "height": 500
                 })
-            else:
+            elif element.tag_name == 'div' and 'jetpack-video-wrapper' in element.get_attribute("class").split():
                 article.append({
-                    "type": "paragraph",
-                    "value": element.text,
+                    "type": "iframe",
+                    "src":  element.find_element(By.TAG_NAME, 'iframe').get_attribute('src'),
+                    "width": 770,
+                    "height": 434,
                 })
-        elif element.tag_name in ("h1", "h2", "h3", "h4", "h5", "h6"):
-            article.append({
-                "type": "header",
-                "size": element.tag_name,
-                "value": element.text
-            })
-        elif element.tag_name == 'div' and 'twitter-tweet' in element.get_attribute("class").split():
-            article.append({
-                "type": "iframe",
-                "src":  element.find_element(By.TAG_NAME, 'iframe').get_attribute('src').replace('platform.twitter.com', 'nitter.pussthecat.org'),
-                "width": 500,
-                "height": 500
-            })
-        elif element.tag_name == 'div' and 'jetpack-video-wrapper' in element.get_attribute("class").split():
-            article.append({
-                "type": "iframe",
-                "src":  element.find_element(By.TAG_NAME, 'iframe').get_attribute('src'),
-                "width": 770,
-                "height": 434,
-            })
-        elif element.tag_name == 'blockquote':
-            article.append({
-                "type": "blockquote",
-                "value":  element.text,
-            })
+            elif element.tag_name == 'blockquote':
+                article.append({
+                    "type": "blockquote",
+                    "value":  element.text,
+                })
 
-    source = None
-    _source_elements = driver.find_elements(By.CLASS_NAME, 'article-source')
-    if len(_source_elements) > 0:
-        source = _source_elements[0].text
+        source = None
+        _source_elements = driver.find_elements(By.CLASS_NAME, 'article-source')
+        if len(_source_elements) > 0:
+            source = _source_elements[0].text
 
-    published = driver.find_element(
-        By.CLASS_NAME, 'article-dates__published').text
+        published = None
+        for item in driver.find_elements(By.CLASS_NAME, 'article-dates__published'):
+            published = item.text
 
-    data = {
-        "title": title,
-        "author": source,
-        "last_updated": published,
-        "article": article
-    }
-    print(Fore.GREEN + 'Fetched ' + Style.RESET_ALL +
-          f"{base_url}/{urllib.parse.unquote(url)}")
+        data = {
+            "title": title,
+            "author": source,
+            "last_updated": published,
+            "article": article
+        }
 
-    return data
+        print('Finished')
+
+        return data
+    except:
+        return
 
 
 def get_recent_articles():
@@ -176,6 +190,8 @@ def get_recent_articles():
         url = urllib.parse.urlparse(entry["link"])
 
         local_link = url.path.strip("/")  # Kill annoying slashes
+        if not local_link.startswith('news/'):
+            continue
 
         r = requests.get(entry['link'])
         soup = BeautifulSoup(r.text, "lxml")
@@ -213,10 +229,9 @@ def get_recent_articles():
 
 
 if __name__ == "__main__":
-
     # page = get_recent_articles()
-    # https://www.aljazeera.net/midan/reality/politics/2022/4/20/%D8%B9%D9%88%D8%AF%D8%A9-%D8%A7%D9%84%D8%BA%D8%B2%D9%88-%D8%A7%D9%84%D8%AA%D9%88%D8%B3%D9%91%D9%8F%D8%B9%D9%8A-%D9%87%D9%84-%D8%AA%D8%AA%D9%81%D8%AC%D9%91%D9%8E%D8%B1-%D8%AD%D8%B1%D9%88%D8%A8
-    page_url = "videos/2022/4/21/%D8%AA%D8%B9%D8%B1%D9%81-%D8%B9%D9%84%D9%89-%D8%AE%D8%B1%D9%8A%D8%B7%D8%A9-%D8%A7%D9%84%D9%85%D9%88%D8%A7%D9%82%D8%B9-%D8%A7%D9%84%D9%86%D9%88%D9%88%D9%8A%D8%A9"
+    # page_url = "news/2022/4/23/بلينكن-بكييف-الأحد-زيلينسكي-يدعو"
+    page_url = "news/2022/4/23/%D8%A8%D9%84%D9%8A%D9%86%D9%83%D9%86-%D8%A8%D9%83%D9%8A%D9%8A%D9%81-%D8%A7%D9%84%D8%A3%D8%AD%D8%AF-%D8%B2%D9%8A%D9%84%D9%8A%D9%86%D8%B3%D9%83%D9%8A-%D9%8A%D8%AF%D8%B9%D9%88"
     page = json.dumps(get_page(page_url), ensure_ascii=False, indent=2)
-    # https://www.aljazeera.net/wp-content/uploads/2022/04/RTXKARMC.jpg?resize=770%2C513
-    # print(page)
+    with open('result.json', "w") as cache_file:
+        cache_file.write(page)
